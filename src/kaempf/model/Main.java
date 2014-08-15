d4966ecb7e8f2dc2ec57630d62a082c1b17af7c2
Maurício Carvalho <mauricio.victor.carvalho@gmail.com>
2014-08-15 09:31:22 -0300

    #5
    criada a classe "Main"

---
+++ src/kaempf/model/Main.java
@@ -0,0 +1,22 @@
+package kaempf.model;
+
+/**
+*
+* mauricio.victor.carvalho@gmail.com
+*
+*/
+public class Main {
+
+	/**
+	 * principal classe do aplicativo, aquela que roda o programa.
+	 */
+	public static void main(String[] args) {
+	
+		/**
+		 * cria um objeto do tipo "InterfaceUsuario" para poder executar o método "executar".
+		 */
+		new InterfaceUsuario().executar();
+
+	}
+
+}
